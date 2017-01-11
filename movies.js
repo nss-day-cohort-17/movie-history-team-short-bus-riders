1f10a4d338ce235815f73eec6765103725867827
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-11 14:22:49 -0600

    troubleshooting api

--- movies.js
+++ movies.js
@@ -1,1 +1,21 @@
-curl http://www.omdbapi.com/?.json
+// API usage
+
+var $Form = $('form'), $Container = $('#container');
+$Container.hide();
+$Form.on('submit', function(p_oEvent){
+    var sUrl, sMovie, oData;
+    p_oEvent.preventDefault();
+sMovie = $Form.find('input').val();
+    sUrl = 'https://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true'
+    $.ajax(sUrl, {
+        complete: function(p_oXHR, p_sStatus){
+            oData = $.parseJSON(p_oXHR.responseText);
+            console.log(oData);
+            $Container.find('.title').text(oData.Title);
+            $Container.find('.plot').text(oData.Plot);
+            $Container.find('.poster').html('<img src="' + oData.Poster + '"/>');
+            $Container.find('.year').text(oData.Year);
+            $Container.show();
+        }
+    });
+});
