be16d14aad6d4c5a53dca936e674741a72297233
Jared Fuller <jaredshane@gmail.com>
2017-01-11 09:41:49 -0600

    added ability add movie poster to HTML after search

--- movies.js
+++ movies.js
@@ -1,5 +1,7 @@
 var str;
 var replaced;
+var movie;
+
 
 $('.btn').click(function(){
   str = $("#movieSearch").val();
@@ -7,11 +9,16 @@ $('.btn').click(function(){
   console.log(str)
   var request = $.ajax({
     method: "GET",
-    url: `http://www.omdbapi.com/?s=${str}&plot=full2&r=json`
+    url: `http://www.omdbapi.com/?t=${str}&plot=full2&r=json`
   }).then(function(data){
+    movie = data;
     console.log(data)
+  }).then(function(){
+    console.log(movie.Title)
+    console.log(movie.Actors)
+    var poster = movie.Poster
+    $('body').append(`<img src="${poster}">`)
   })
-  console.log(request)
 })
 
 
