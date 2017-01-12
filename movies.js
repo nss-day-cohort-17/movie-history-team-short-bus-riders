a4e1c34400180b81ce8f20e786ebfde8802733e6
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-11 21:48:40 -0600

    firebase api functionality

--- movies.js
+++ movies.js
@@ -1,21 +1,66 @@
 // API usage
 
-var $Form = $('form'), $Container = $('#container');
-$Container.hide();
-$Form.on('submit', function(p_oEvent){
-    var sUrl, sMovie, oData;
-    p_oEvent.preventDefault();
-sMovie = $Form.find('input').val();
-    sUrl = 'https://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true'
-    $.ajax(sUrl, {
-        complete: function(p_oXHR, p_sStatus){
-            oData = $.parseJSON(p_oXHR.responseText);
-            console.log(oData);
-            $Container.find('.title').text(oData.Title);
-            $Container.find('.plot').text(oData.Plot);
-            $Container.find('.poster').html('<img src="' + oData.Poster + '"/>');
-            $Container.find('.year').text(oData.Year);
-            $Container.show();
-        }
-    });
+// var $Form = $('form'), $Container = $('#container');
+// $Container.hide();
+// $Form.on('submit', function(p_oEvent){
+//     var sUrl, sMovie, oData;
+//     p_oEvent.preventDefault();
+// sMovie = $Form.find('input').val();
+//     sUrl = 'https://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true'
+//     $.ajax(sUrl, {
+//         complete: function(p_oXHR, p_sStatus){
+//             oData = $.parseJSON(p_oXHR.responseText);
+//             console.log(oData);
+//             $Container.find('.title').text(oData.Title);
+//             $Container.find('.plot').text(oData.Plot);
+//             $Container.find('.poster').html('<img src="' + oData.Poster + '"/>');
+//             $Container.find('.year').text(oData.Year);
+//             $Container.show();
+//         }
+//     });
+// });
+
+// click function for query
+
+var title;
+var year;
+
+$("#omdb_submit").click(function(){
+    //Gather values from the other fields and store them into variables.
+    var title = $("#title").val();
+    var year = $("#year").val();
+    // Concatenate those variables to a query string.
+    var queryString = "http://www.omdbapi.com/?t=" + title + "&y=" + year + "&plot=short&r=json";
 });
+
+// Put that query string into the AJAX request
+        $.ajax({
+            url: queryString, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+            method: 'GET'
+        }).done(function(response) {
+            if(response.length < 1) {
+                // Output error message into output container
+                $("#output").html("Sorry, no movies were found :(");
+            } else {
+                // Output data into output container
+                $("#output").html(JSON.stringify(response));
+
+                // create an HTML element that will hold all of the prettified elements
+                var movieContainer = $('<div class="movie_Container">');
+                // Append the movie container to the existing container
+                $(".pretty_movie_output").append(movieContainer);
+
+                // Go through each property of the object and create/input the data from the object
+                for(var prop in response) {
+                    var element;
+                    if (prop == "Poster" && response[prop] != "N/A") {
+                        element = $("<img class='pretty'>").attr("src", response[prop]);
+                    } else {
+                        element = $("<h3 class='pretty'>").text(prop + ": " + response[prop]);
+                    }
+
+                    movieContainer.append(element);
+                }
+            }
+        });
+    });
