2c99b67af965b932962736b9b769054979ec5a1e
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-13 08:46:43 -0600

    merged firebase code

--- firebase.js
+++ firebase.js
@@ -1,39 +1,39 @@
-$('.main-page form').submit((e) => {
-  var task = $('.main-page input[type="text"]').val()
-  var uid = firebase.auth().currentUser.uid
-  $.post(
-    `https://movies-by-short-bus.firebaseio.com/${uid}.json`,
-    JSON.stringify({ task: task })
-  ).then(res => console.log(res.name))
+// $('.main-page form').submit((e) => {
+//   var task = $('.main-page input[type="text"]').val()
+//   var uid = firebase.auth().currentUser.uid
+//   $.post(
+//     `https://movies-by-short-bus.firebaseio.com/${uid}.json`,
+//     JSON.stringify({ task: task })
+//   ).then(res => console.log(res.name))
 
-  e.preventDefault()
-})
+//   e.preventDefault()
+// })
 
-//save a movie
+// //save a movie
 
-function saveMovie(e){
-    // console.log("new log",newMovieData)
-    $.ajax({
-        accept: "application/json",
-        type: 'POST',
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json",
-        data: JSON.stringify(newMovieData)
-    });
-    clearMovie()
-}
+// function saveMovie(e){
+//     // console.log("new log",newMovieData)
+//     $.ajax({
+//         accept: "application/json",
+//         type: 'POST',
+//         contentType: "application/json; charset=utf-8",
+//         dataType: "json",
+//         url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json",
+//         data: JSON.stringify(newMovieData)
+//     });
+//     clearMovie()
+// }
 
-//pull saved movies from firebase
+// //pull saved movies from firebase
 
-function myMovies(){
-    // console.log("new log",newMovieData)
-    $.ajax({url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json"})
-        .done(function(e) {
+// function myMovies(){
+//     // console.log("new log",newMovieData)
+//     $.ajax({url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json"})
+//         .done(function(e) {
 
-        populateMyMoviesPage(e) // <--send saved movies to function populateMyMoviesPage
+//         populateMyMoviesPage(e) // <--send saved movies to function populateMyMoviesPage
 
-        // console.log("your saved movies are:", e)
-})
+//         // console.log("your saved movies are:", e)
+// })
 
-}
+// }
--- main.js
+++ main.js
@@ -69,3 +69,43 @@ function showHideStuff() {
   $('.registrationButtons').addClass('hidden')
   $('.datawrapper').removeClass('hidden')
 }
+
+$('.main-page form').submit((e) => {
+  var task = $('.main-page input[type="text"]').val()
+  var uid = firebase.auth().currentUser.uid
+  $.post(
+    `https://movies-by-short-bus.firebaseio.com/${uid}.json`,
+    JSON.stringify({ task: task })
+  ).then(res => console.log(res.name))
+
+  e.preventDefault()
+})
+
+//save a movie
+
+function saveMovie(e){
+    // console.log("new log",newMovieData)
+    $.ajax({
+        accept: "application/json",
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json",
+        data: JSON.stringify(newMovieData)
+    });
+    clearMovie()
+}
+
+//pull saved movies from firebase
+
+function myMovies(){
+    // console.log("new log",newMovieData)
+    $.ajax({url: "https://movie-history-team-short-bus-riders.firebaseio.com/.json"})
+        .done(function(e) {
+
+        populateMyMoviesPage(e) // <--send saved movies to function populateMyMoviesPage
+
+        // console.log("your saved movies are:", e)
+})
+
+}
